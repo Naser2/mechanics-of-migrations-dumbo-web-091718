7d3217c0e31533da26dfa6c7f4a470d9c63fe8d0
github username <github email address>
2018-09-29 00:14:05 -0400

    boss says it's actually time to add that column 'add_favorite'

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -3,4 +3,8 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration
   def change
     add_column :artists, :favorite_food, :string
   end
-end 
+end
+
+# 2.4.2 :001 > Artist.column_names
+#  => ["id", "name", "genre", "age", "hometown"]
+# 2.4.2 :002 >
